acaf6a2be8cff310d6000b450dbc8e14527918e1
Mgl <miguel.alvarez1@udea.edu.co>
2023-07-26 11:18:36 -0500

    Creacionales

--- Notas Java Avanzado/src/Main.java
+++ Notas Java Avanzado/src/Main.java
@@ -88,16 +88,195 @@ Para desde cmd ejecutar comando http y que nos muestre en crudo la respuesta:
 curl localhost:8080
 hola
 
+*/
+
+/*Postman
+App para apis ejecutar apis desde postman para testearlas
+Tiene variables posibles de crear, ejemplo url para no estar colocando siempre el url tan largo
+
+Test: sirve para pruebas y ver si al respuesta de la api es lo que yo quiero que responda
+pm.test("Status code is 200", function () {
+  pm.response.to.have.status(200);
+});
+
+ */
+
+/*Patrones de diseño -Patrones Creacionales:
+Estandarizar el desarrollo para que todos programemos igual y se entienda el codigo
+
+
+Singleton: 
+Patron de diseño para que no se creen instancias infinitas(se cree y se cree nuevos objetos de una clase)
+//a cualquier clase se le tiene que crear un getInstance, un constructor privado y una instanciacion privada inicial base 
+//hacer que funcione el singleton:
+    public static class Singleton{
+        private static Singleton instance; //creamos una variable privada de tipo singleton
+        private Singleton (){} //creamos el constructor privado para que no se pueda crear desde afuera usando new singleton
+        
+        //la primera vez que llamamos getInstance si dejara crer/instanciar un objeto singleton
+        //pero si ya existe un objeto singleton creado, no dejara crear otro y devolvera el que ya existe
+        public static Singleton getInstance(){
+            if (instance == null){
+                instance = new Singleton();
+            } 
+            return instance;
+        }
+        //para llamar al singleton desde afuera
+        //Singleton singleton = Singleton.getInstance();
+    }
+
+Factory:
+Patron de diseño para crear objetos de una clase sin tener que usar new sino que los invocamos desde 
+el factory y el factory se encarga de ver sí es un objeto u otro y crearlo
+public class Factory {
+        private Precio precio;
+    
+        private Factory() {};
+        //modificamos el constructor para que se encargue de crear este objeto o este otro objeto
+        public Factory(String pais) {
+            if (pais.equalsIgnoreCase("España")) {
+                this.precio = new PrecioEUR();
+            } else {
+                this.precio = new PrecioUSD();
+            }
+        }
+        //para que en Main no toque poner factory.precio.getPrecio() que es más largo
+        public double getPrecio() {
+            return precio.getPrecio();
+        }
+    }
+    interface Precio {
+        double getPrecio();
+    }
+    class PrecioEUR implements Precio {
+        public double getPrecio() {
+            return 1.3;
+        }
+    }
+    class PrecioUSD implements Precio {
+        public double getPrecio() {
+            return 0.94;
+        }
+    }
+Factory factory = new Factory("USA");
+        System.out.println(factory.getPrecio());
+
+
+Builder:
+Es para construir clases que tienen muchos atributos y no queremos tener que poner todos los atributos
+uno por uno en el constructor, sino que los pasamos todos en el builder y el builder se encarga de crear
+
+Es parecido a cuando usamos los stream .map.reduce.filter etc nos van devolviendo el mismo tipo de objeto
+que les mandamos pero lo van modificando
+
+public class CocheBuilder {
+    Vehiculo vehiculo;  //creamos el objeto que vamos a ir modificando
+
+    private CocheBuilder() {};
+
+    public CocheBuilder(String marca) {
+        vehiculo = new Vehiculo(marca);
+        vehiculo.marca = marca;
+    }
+    //le ponemos solo los setters sin getters porque el builder solo construye no devuelve
+    public CocheBuilder setPuertas(int puertas) {
+        vehiculo.puertas = puertas;
+        return this;        //return this para devolver el mismo objeto que estamos modificando
+        //pero con el atributo que le pasamos modificado
+    }
+
+    public CocheBuilder setMotor(String motor) {
+        vehiculo.motor = motor;
+        return this;
+    }
+
+    public CocheBuilder setTipo(String tipo) {
+        vehiculo.tipo = tipo;
+        return this;
+    }
+    //devuelve el objeto vehiculo que ya tiene todos los atributos modificados
+    public Vehiculo build() {
+        return this.vehiculo;
+    }
+}
+public class Vehiculo {
+    public String marca;
+    public String tipo;
+    public String motor;
+    public int puertas;
+
+    private Vehiculo() {}
+
+    public Vehiculo(String marca) {
+        this.marca = marca;
+    }
+    //el objeto solo tendra getters porque para crearlo se debera usar el constructor
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public String getMotor() {
+        return motor;
+    }
+
+    public int getPuertas() {
+        return puertas;
+    }
+}
+
+//en el Main, le pasamos todos los atributos pero con .set   
+luego .build() para que devuelva el objeto que queremos no el objetoBuilder que estaban manejando los setters
+Vehiculo coche = new CocheBuilder("Filostro")
+    .setTipo("Diesel")
+    .setMotor("Combustion")
+    .setPuertas(5)
+    .build();
+
+Prototype:
+Es un patron para poder clonar un objeto con todas sus propiedades y no tener que crearlo de nuevo
+usando implements Cloneable que se encarga de todo eso en java
+
+public class Vehiculo implements Cloneable {
+    public String marca;
+    public String modelo;
+    int anyoFabricacion;
+
+    public Vehiculo() {}
+
+    public Vehiculo clonar() throws CloneNotSupportedException {
+        return (Vehiculo) this.clone();
+    }
+}
+En el main solo sería llamar al metodo clonar teniendo en cuenta que puede lanzar una excepcion
+entonces usar try catch y listo
+
+
+*/
+
+/* Patrones Estructurales
+
+Decorator:
+
+
+
 
 */
 
 
-
 public class Main {
+    
+    
+    
+    
 
 
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        
+
     }
 
 
